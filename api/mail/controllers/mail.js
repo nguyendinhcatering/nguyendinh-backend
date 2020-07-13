593aa22b795fda51ed3e479bdf587bd32b2768fe
Kevin Nguyen <k.nguyen0125@gmail.com>
2020-07-13 23:11:28 +0700

    feat: addons

--- api/mail/controllers/mail.js
+++ api/mail/controllers/mail.js
@@ -1,4 +1,6 @@
 "use strict";
+const Axios = require("axios");
+const axios = Axios.create({ baseURL: process.env.MAILER_URL });
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
@@ -9,6 +11,17 @@ module.exports = {
   contact: async (ctx) => {
     console.log(ctx.request.body);
 
-    return "hi";
+    return axios.post("/", {
+      template: "contact-us",
+      smtpOptions: {
+        to: "kevin.nguyen0125@gmail.com",
+      },
+      variables: {
+        email: ctx.request.body.email,
+        name: ctx.request.body.name,
+        message: ctx.request.body.message,
+        address: ctx.request.body.address,
+      },
+    });
   },
 };
