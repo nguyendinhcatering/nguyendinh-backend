033bdefaad086112b4386ecb7a7ab894926b0699
Kevin Nguyen <k.nguyen0125@gmail.com>
2020-11-13 22:27:20 +0700

    feat: add published to news item

--- api/news-item/controllers/news-item.js
+++ api/news-item/controllers/news-item.js
@@ -1,8 +1,37 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services["news-item"].search(ctx.query);
+    } else {
+      entities = await strapi.services["news-item"].find(ctx.query);
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models["news-item"] })
+    );
+  },
+
+  async findByNewsType(ctx) {
+    console.log(ctx);
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services["news-item"].search(ctx.query);
+    } else {
+      entities = await strapi.services["news-item"].find(ctx.query);
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models["news-item"] })
+    );
+  },
+};
--- api/news-type/controllers/news-type.js
+++ api/news-type/controllers/news-type.js
@@ -1,8 +1,48 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services["news-type"].search(ctx.query, []);
+    } else {
+      entities = await strapi.services["news-type"].find(ctx.query, []);
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models["news-type"] })
+    );
+  },
+
+  async findByNewsType(ctx) {
+    console.log(ctx.params);
+    console.log(ctx.query);
+    const entity = await strapi.services["news-type"].findOne(
+      {
+        id: ctx.params.id,
+      },
+      []
+    );
+    entity["news_items"] = await strapi.services["news-item"].find(
+      {
+        _sort: "updated_at:ASC",
+        _where: {
+          news_types: ctx.params.id,
+          published: true,
+        },
+        _limit: ctx.query.limit || 100,
+        _start: (ctx.query.start || 0) * (ctx.query.limit || 100),
+      },
+      ["banners"]
+    );
+
+    return sanitizeEntity(entity, { model: strapi.models["news-type"] });
+  },
+};
