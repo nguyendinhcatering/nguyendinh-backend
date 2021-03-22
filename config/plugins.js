3c7cc13f62a7379c1ae796136b8c86261e168413
Kien Nguyen <k.nguyen0125@gmail.com>
2021-03-22 22:50:14 +0700

    feat: allow upload to upload to s3

--- config/plugins.js
+++ config/plugins.js
@@ -14,7 +14,7 @@ module.exports = ({ env }) => {
       provider: "s3-cloudfront",
       providerOptions: {
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
         region: env("AWS_REGION"),
         params: {
           Bucket: env("AWS_BUCKET"),
